9e69a0e6e998a0ca82f014d746b89a5ecf13fa01
root <root@localhost.localdomain>
2018-06-03 23:49:08 +0530

    fro command output

--- Socket_Programming/receiver.py
+++ Socket_Programming/receiver.py
@@ -3,6 +3,7 @@
 #import modeule
 import socket
 import os
+import commands
 
 #for make connection between sender and receiver
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
@@ -13,10 +14,13 @@ s.bind(("127.0.0.1",7777))
 for i in range(10000):
 	#to get the data from sender side
 	data = s.recvfrom(10000)
-	print data
-	#to give the message to sender in voice form
-	msg = os.system('echo "hii" | festival --tts')
-	#to send message to sender
-	s.sendto(str(msg),data[1])
+	#to get the output of command
+	check=commands.getstatusoutput(data[0])
+	if check==0:
+		#to send the command output to sender
+		s.sendto(check[1],data[1])
+	else:
+		#to send the output if command not found 
+		s.sendto(check[1],data[1])
 	
 	
